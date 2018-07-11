3c07f1653c8aece4bf3b2b24b3963d7dfc9246b6
Ryan Rich <ryan@datica.com>
2018-07-11 08:56:19 -0700

    Finishing mobile menu

--- source/assets/js/all.js
+++ source/assets/js/all.js
@@ -17,30 +17,42 @@ $(window).on("load",function(){
 // Nav wrapper
 
 $(window).on("scroll", function (event){
+
   var nw = $("#nav-wrapper");
   var sc = $(window).scrollTop();
 
-  if (sc >= 100){
+  if ((sc >= 100) && ($(window).width() > 767)){
     nw.addClass("fixIt");
   } else {
     nw.removeClass("fixIt");
   }
 });
 
-$(window).on("load", function(){
-  var w = $(".col-xs-3").width();
-  var n = $("#nav-wrapper");
+// Grab the width of the outer collumn and apply it to the fixed class so it has a width
+
+$(window).on("load resize", function(){
+  var w  = $(".col-xs-3").width();
+  var n  = $("#nav-wrapper");
+  var m  = $(".main-nav");
+  var mm = $(".mobile-menu-button");
+  var l  = $(".left-nav-mobile");
+  var ll = $(".left-nav");
+  var b  = $(".close-mobile-menu-button")
 
   console.log(w);
 
   n.css({"width" : w - 16});
-});
 
-$(window).on("resize", function(){
-  var w = $(".col-xs-3").width();
-  var n = $("#nav-wrapper");
+  mm.on("click", function(){
+    l.addClass("showhelper");
+    ll.addClass("showhelper");
+    b.addClass("showhelper");
+  });
 
-  console.log(w);
+  b.on("click", function(){
+    l.removeClass("showhelper");
+    ll.removeClass("showhelper");
+    b.removeClass("showhelper");
+  });
 
-  n.css({"width" : w - 16});
 });
